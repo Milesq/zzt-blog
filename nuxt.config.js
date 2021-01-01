94f61b6500f0c554244e4a6059d6c80711286850
Milesq <wmilosz88@gmail.com>
2021-01-01 15:59:36 +0100

    feat: disable dark theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors';
-
 export default {
   ssr: false,
   head: {
@@ -21,20 +19,6 @@ export default {
   modules: ['@nuxtjs/pwa', '@nuxt/content'],
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
   },
   storybook: {
     stories: ['~/stories/**/*.stories.js'],
