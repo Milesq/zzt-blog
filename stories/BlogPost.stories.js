eae756df227c0eb34afcf2a6b02b049232ed887e
Milesq <wmilosz88@gmail.com>
2021-01-01 15:18:06 +0100

    chore(storybook): fulfill stories

--- stories/BlogPost.stories.js
+++ stories/BlogPost.stories.js
@@ -2,4 +2,7 @@ export default {
   title: 'Post',
 }
 
-export const NuxtWebsite = () => '<BlogPost />'
+export const Full = () =>
+  '<v-app><CardView title="Lorem ipsum" desc="abc" /></v-app>'
+export const WithoutProps = () => '<v-app><CardView /></v-app>'
+export const WithTitle = () => '<v-app><CardView title="Lorem ipsum" /></v-app>'
