48fbd1b1d0f131094971a02af260e62a95e9b6b4
Milesq <wmilosz88@gmail.com>
2021-01-02 16:05:05 +0100

    feat: display every post from content/posts

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,16 @@
     <v-row justify="center" align="center">
       <v-col cols="12" sm="8">
         <v-container>
-          <BlogPost />
+          <BlogPost
+            v-for="(post, i) in posts"
+            :key="i"
+
+            :title="post.title"
+            :description="post.description"
+            :createdAt="new Date(post.createdAt)"
+            :category="post.category"
+            :src="post.image"
+          />
         </v-container>
       </v-col>
     </v-row>
@@ -13,5 +22,14 @@
 <script>
 export default {
   name: 'Post',
+  async asyncData({ $content }) {
+    const fields = ['title', 'image', 'category', 'description', 'createdAt'];
+
+    const posts = await $content('posts').only(fields).fetch();
+
+    return {
+      posts,
+    };
+  },
 };
 </script>
