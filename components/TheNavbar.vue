fe9554013bee2df7eb012763484f47c2b42f5b96
Milesq <wmilosz88@gmail.com>
2021-01-13 16:30:51 +0100

    feat: make nav buttons clickable

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -14,12 +14,39 @@
       <v-icon>mdi-magnify</v-icon>
     </v-btn>
 
-    <v-btn icon>
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
+    <v-tooltip bottom>
+      <template #activator="{ on }">
+        <v-btn
+          v-on="on"
+          icon
+          target="blank"
+          href="https://zwolnienizteorii.pl/"
+        >
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+      </template>
 
-    <v-btn icon>
-      <v-icon>mdi-dots-vertical</v-icon>
-    </v-btn>
+      <span>Polub naszą stronę nasz projekt na ZZT</span>
+    </v-tooltip>
+
+    <v-tooltip bottom>
+      <template #activator="{ on }">
+        <v-btn @click="changeTheme" v-on="on" icon>
+          <v-icon>mdi-brightness-6</v-icon>
+        </v-btn>
+      </template>
+
+      <span>Zmień Tło</span>
+    </v-tooltip>
   </v-app-bar>
 </template>
+
+<script>
+export default {
+  methods: {
+    changeTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
+</script>
