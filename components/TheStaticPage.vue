53922e481035352271a11c6901b586d83c5555c6
Milesq <wmilosz88@gmail.com>
2021-01-16 12:36:04 +0100

    feat: add contact page

---
+++ components/TheStaticPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mb-16">
+    <nuxt-content :document="mainContent" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  async fetch() {
+    this.mainContent = await this.$content(this.name).fetch();
+  },
+  data: () => ({
+    mainContent: null,
+  }),
+};
+</script>
